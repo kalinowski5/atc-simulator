513e0e74ee79641520176a6a60afbcb275934c2b
Michał Kalinowski <kalinowski5@gmail.com>
2022-08-20 21:53:41 +0200

    Kick off the project

---
+++ src/main/java/org/miasta/atcsimulator/AtcSimulatorApplication.java
@@ -0,0 +1,13 @@
+package org.miasta.atcsimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtcSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtcSimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/miasta/atcsimulator/AtcSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.miasta.atcsimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtcSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
